bcf5dbbd9cf922181a68110b37d8f55f6ca5198d
cliffordgail01 <davisbeacon23@gmail.com>
2023-12-29 14:00:16 -0500

    getting up to speed

---
+++ foo.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    def foo(bar: str = 1):
+        print(bar)
+
+    foo()
--- main.py
+++ main.py
@@ -1,13 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
+from enum import Enum
+from datetime import datetime
+
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Title(Enum):
+    CHIEF = 'ceo'
+    OPERATIONS = 'coo'
+    TECH = 'cto'
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+class Recruit(BaseModel):
+    name: str
+    total_comp: int = 0
+    start_date: datetime | None = Field(default_factory=datetime.now)
+    __recruits__: list = []
+
+    @classmethod
+    def add_recruit(cls, recruit: 'Recruit'):
+        cls.__recruits__.append(recruit)
+
+    @classmethod
+    def all(cls):
+        return cls.__recruits__
+
+
+@app.get("/team/ceo")
+async def get_ceo():
+    return {
+        Title.CHIEF: "Gail Cliff"
+    }
+
+
+@app.get("/team")
+async def get_team_member(title: Annotated[Title, Query()] = None):
+    return {
+        title or "t": "John Doe"
+    }
+
+
+@app.get("/download/{file:path}")
+async def download(file: str):
+    return file
+
+
+@app.post('/team/hire')
+def recruit(member: Recruit) -> Recruit:
+    Recruit.add_recruit(member)
+    return member
+
+
+@app.get('/team/recruits')
+def get_recruits() -> list[Recruit]:
+    return Recruit.all()
